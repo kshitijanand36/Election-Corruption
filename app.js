ff10a88eeecdfd9070d6cb6f13435534a990ab1a
Kshitij Anand <kshitijanand2@gmail.com>
2021-01-31 18:35:09 +0530

    Final changes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-
+const app = express();
 // Load the full build.
 var _ = require('lodash');
 
@@ -13,9 +13,7 @@ var at = require('lodash/at');
 var curryN = require('lodash/fp/curryN');
 
 const homeStartingContent = "All the reports posted have been listed down here. You can read any of the reports by clicking on read more.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const app = express();
 
 posts = [];
 
@@ -35,7 +33,7 @@ app.get("/latest", function(req , res){
   res.render("content" , {content : posts , starting_content : homeStartingContent });
 });
 
-app.get("/blogs/:topic" , function(req , res){
+app.get("/reports/:topic" , function(req , res){
 
   var reqd = _.lowerCase( req.params.topic);
 
