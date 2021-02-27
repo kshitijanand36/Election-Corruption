867995c607b495142ca24b9fd8c6d75ffc1b0823
Karan Kaira <46125192+KaranKaira@users.noreply.github.com>
2021-02-28 02:09:58 +0530

    Delete Credentials.js

--- Credentials.js
+++
@@ -1,6 +0,0 @@
-
-const Username = 'admin-karan';
-const Password = 'test123';
-const link = 'mongodb+srv://'+ Username + ':' + Password + '@cluster0.qpybj.mongodb.net/ElectionCorruption';
-
-exports.link = link;
