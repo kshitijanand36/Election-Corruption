edc3c8344248a118ad070f794e5668245fe42d95
Karan <kairakaran362@gmail.com>
2021-02-28 01:48:03 +0530

    Minor change

---
+++ Credentials.js
@@ -0,0 +1,6 @@
+
+const Username = 'admin-karan';
+const Password = 'test123';
+const link = 'mongodb+srv://'+ Username + ':' + Password + '@cluster0.qpybj.mongodb.net/ElectionCorruption';
+
+exports.link = link;
