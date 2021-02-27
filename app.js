8bdc3a4e31bd0879b05351efd2ababab37c2e9e2
Karan <kairakaran362@gmail.com>
2021-02-28 01:21:39 +0530

    Added  DB and Delete Update Features , Now merge with main branch

--- app.js
+++ app.js
@@ -4,75 +4,130 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const app = express();
-// Load the full build.
-var _ = require('lodash');
+const  _ = require('lodash');
+const mongoose  = require("mongoose");
+const credentials = require(__dirname + '/Credentials.js');
+
+// mongoose.connect('mongodb://localhost:27017/ElectionCorruption',{
+//   useNewUrlParser : true,
+//   useUnifiedTopology:true
+// });
+
+mongoose.connect(credentials.link,{
+  useNewUrlParser: true,   
+  useUnifiedTopology: true
+});
 
 
+ const postSchema = new mongoose.Schema({
+   post_title :   { 
+     type : String,
+     required : true
+   },
+   post_body : {
+     type : String , 
+     required : true
+   }
+ });
+
+ const Post = mongoose.model('Post',postSchema);
 
 const homeStartingContent = "All the reports posted have been listed down here. You can read any of the reports by clicking on read more.";
 
 
-posts = [];
+
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get("/" , function(req , res){
+app.get("/" , (req , res)=>{
 
   res.sendFile(__dirname +  "/index.html");
 
-})
-
-app.get("/latest", function(req , res){
-
-  res.render("content" , {content : posts , starting_content : homeStartingContent });
 });
 
-app.get("/reports/:topic" , function(req , res){
+app.get("/latest", (req , res)=>{
 
-  var reqd = _.lowerCase( req.params.topic);
+  Post.find({},(err,docs)=>{
+    res.render('content',{
+      starting_content : homeStartingContent, 
+      posts : docs
+    }); 
+  })
+});
 
-  console.log(reqd);
-  for(let i = 0 ;  i< posts.length ; i++){
-    console.log(_.lowerCase(posts[i].post_title));
-    if(_.lowerCase(posts[i].post_title) == reqd){
-      res.render("post" , {post_title :posts[i].post_title , post_content : posts[i].post_body })
-    }
-  }
+app.get("/reports/:id" , (req , res)=>{
+
+  const  reqd =  req.params.id;
+
+  
+  Post.findById(reqd , (err,doc)=>{
+      res.render('post',{thispost : doc});
+  });
 
 
 
-  res.render("error");
 })
 
-app.get("/home" , function(req , res){
+app.get("/home" , (req , res)=>{
 
   res.redirect("/");
 })
 
 
 
-app.get("/report" , function(req , res){
+app.get("/report" , (req , res)=>{
 
   res.render("compose");
 });
 
 
-app.post("/compose" , function(req , res){
-  const post = {
+app.post("/compose" , (req , res)=>{
+
+  const newPost = new Post({
     post_title : req.body.post_title,
     post_body : req.body.post_body
-  }
+  });
+  newPost.save(()=>{
+    res.redirect('/latest');
+  })
 
-  // console.log(post);
-
-  posts.push(post);
-
-  res.redirect("/latest");
 });
 
+// update
+app.get('/update/:id' , (req,res)=>{
+
+  const reqd = req.params.id;
+
+  Post.findById(reqd, (err,doc)=>{
+    res.render('update' , {thispost : doc});
+  })
+});
+
+app.post('/update/:id' , (req,res)=>{
+  const reqd = req.params.id; 
+
+  const updatedTitle = req.body.post_title;
+  const updatedBody = req.body.post_body;
+
+  Post.findByIdAndUpdate(reqd,{post_title : updatedTitle , post_body : updatedBody} , (err,doc)=>{
+    res.redirect('/reports/' + reqd);
+  });
+});
+
+
+// delete
+app.post('/delete/:id',(req,res)=>{
+
+  const reqd = req.params.id;
+
+  Post.findByIdAndRemove(reqd, (err)=>{
+    res.redirect('/latest');
+  });
+
+});
 app.listen(process.env.PORT || 3000 , function() {
   console.log("Server started on port 3000");
 });
