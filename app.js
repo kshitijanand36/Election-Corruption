2baa07c06177a7fb049e9d7409fd0fb23ea21807
Karan <kairakaran362@gmail.com>
2021-03-12 18:59:36 +0530

    Refactor

--- app.js
+++ app.js
@@ -29,6 +29,8 @@ app.use(HomeRouter);
 
 //! Set View Engine 
 app.set('view engine', 'ejs');
+
+
 //! Serve static files
 app.use(express.static("public"));
 
@@ -49,106 +51,7 @@ app.use(express.static("public"));
 // });
 
 
-//  const postSchema = new mongoose.Schema({
-//    post_title :   { 
-//      type : String,
-//      required : true
-//    },
-//    post_body : {
-//      type : String , 
-//      required : true
-//    }
-//  });
-
-//  const Post = mongoose.model('Post',postSchema);
-
-
-
-
-
-
-
-// app.get("/" , (req , res)=>{
-
-//   res.sendFile(__dirname +  "/index.html");
-
-// });
-
-// app.get("/latest", (req , res)=>{
-
-//   Post.find({},(err,docs)=>{
-//     res.render('content',{
-//       starting_content : homeStartingContent, 
-//       posts : docs
-//     }); 
-//   })
-// });
-
-// app.get("/reports/:id" , (req , res)=>{
-
-//   const  reqd =  req.params.id;
-
-//   Post.findById(reqd , (err,doc)=>{
-//       res.render('post',{thispost : doc});
-//   });
-// })
-
-// app.get("/home" , (req , res)=>{
-//   res.redirect("/");
-// })
-
-
-
-// app.get("/report" , (req , res)=>{
-//   res.render("compose");
-// });
-
-
-// app.post("/compose" , (req , res)=>{
-
-//   const newPost = new Post({
-//     post_title : req.body.post_title,
-//     post_body : req.body.post_body
-//   });
-//   newPost.save(()=>{
-//     res.redirect('/latest');
-//   })
-
-// });
-
-//* update
-// app.get('/update/:id' , (req,res)=>{
-
-//   const reqd = req.params.id;
-
-//   Post.findById(reqd, (err,doc)=>{
-//     res.render('update' , {thispost : doc});
-//   })
-// });
-
-// app.post('/update/:id' , (req,res)=>{
-//   const reqd = req.params.id; 
-
-//   const updatedTitle = req.body.post_title;
-//   const updatedBody = req.body.post_body;
-
-//   Post.findByIdAndUpdate(reqd,{post_title : updatedTitle , post_body : updatedBody} , (err,doc)=>{
-//     res.redirect('/reports/' + reqd);
-//   });
-// });
-
-
-//* delete
-// app.post('/delete/:id',(req,res)=>{
-
-//   const reqd = req.params.id;
-
-//   Post.findByIdAndRemove(reqd, (err)=>{
-//     res.redirect('/latest');
-//   });
-// });
-
-app.listen(process.env.PORT || 3000 , function() {
+app.listen(process.env.PORT || 3000 , ()=> {
   console.log("Server started on port 3000");
 });
 
--- routes/Report.js
+++ routes/Report.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-
-
 router.get("/report" , (req , res)=>{
     res.render("compose");
 });
